fad5c4f756d22dfe59a15f30886427ea316a6a62
AashishBadal <aashishbadal4@gmail.com>
2025-12-30 11:41:23 +0545

    completed editor panel

---
+++ client/src/components/LoaderSteps.tsx
@@ -0,0 +1,15 @@
+import { ScanLineIcon, Square } from "lucide-react"
+
+const steps =[
+    {icon:ScanLineIcon,label:"Analyzing your request"},
+    {icon:Square,label:"Generating layout Structure"},
+    {icon:ScanLineIcon,label:"Analyzing your request"},
+]
+
+const LoaderSteps = () => {
+  return (
+    <div>LoaderSteps</div>
+  )
+}
+
+export default LoaderSteps
