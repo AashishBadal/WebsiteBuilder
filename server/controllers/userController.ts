7c516440bb7f9e828f02c0a582065997185835fc
AashishBadal <aashishbadal4@gmail.com>
2026-01-02 11:28:40 +0545

    controller function to toggle project publish

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -243,3 +243,33 @@ export const getUserProjects = async (req: Request, res: Response) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+//controller function to toggle project publish
+export const togglePublish = async (req: Request, res: Response) => {
+  try {
+    const userId = req.userId;
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const { projectId } = req.params;
+
+    const project = await prisma.websiteProject.findUnique({
+      where:{id:projectId,userId}
+    })
+
+    if(!project){
+      return res.status(404).json({ message: "Project not found" });
+    }
+    
+    await prisma.websiteProject.update({
+      where:{id:projectId},
+      data:{isPublished:!project.isPublished}
+    })
+
+    res.json({ message:project.isPublished ? 'Project Unpublished':'Project Published Successfully' });
+  } catch (error: any) {
+    console.log(error.code || error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
